42db4a72c1cbd6a49b52cbbb808f25f32f0f59a6
github username <github email address>
2020-08-06 15:55:33 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -12,4 +12,36 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
-});
+    
+  BASE_URL = "http://localhost:3000/toys"
+   const getToys = ()=>{
+    fetch(BASE_URL)
+    .then(response => response.json())
+    // .then(toysCollection => console.log(toysCollection))
+    .then(toys => {
+      toys.forEach(toy =>{
+        renderToy(toy)}
+        )
+      })
+    }
+ 
+    function renderToy(toy){
+      console.log(toy, "toy")
+      const divContainer = document.querySelector("#toy-collection")
+      const toyContainer = document.createElement('div')
+      divContainer.appendChild(toyContainer)
+      toyContainer.innerHTML = `
+      <div class="card">
+      <h2>${toy.name}</h2>
+      <img src=${toy.image} class="toy-avatar" />
+      <p> ${toy.likes} Likes </p>
+      <button class="like-btn">Like <3</button>
+      </div>
+      `
+      // debugger
+    }
+  
+
+  getToys()
+  
+})
