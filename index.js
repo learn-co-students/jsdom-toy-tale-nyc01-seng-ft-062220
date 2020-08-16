ea8562663c277d7510e0f81fe363ac59ffd739a8
catd825 <catd825@gmail.com>
2020-08-15 20:52:05 -0400

    Done.

--- index.js
+++ index.js
@@ -1,38 +1,111 @@
-// 1. get toys 
-// 2. load cards from database
-// 3. render/create cards
-// 4. insert cards onto page in div class
+document.addEventListener("DOMContentLoaded", function(){
 
-const getToys = () => {
-    fetch("http://localhost:3000/toys")
-    .then(response => response.json())
-    .then(toysCollection => renderCards(toysCollection))
-}
+    const toysUrl = "http://localhost:3000/toys/"
 
-function renderCards(toysCollection) {
-    toysCollection.forEach (function (toysObj) {
-        renderToy(toysObj)
-    })
-}
+    getToys = () => {
+        fetch(toysUrl)
+        .then(response => response.json())
+        .then(toyCollection => renderAllToys(toyCollection))
+    }
 
-function renderToy(toysObj){
-    const toyCard = document.createElement('div')
-    toyCard.innerHTML = `
-    <div class="card">
-    <h2>${toysObj.name}</h2>
-    <img src="${toysObj.image}" class="toy-avatar" />
-    <p>${toysObj.likes}</p>
-    <button class="like-btn">Like <3</button>
-  </div>
-    `
-insertCards(toyCard)
-}
+    const renderAllToys = (toyCollection) => {
+        toyCollection.forEach(toy => renderSingleToy(toy))
+    }
+
+    const renderSingleToy = (toyObj) => {
+        const toyCollectionDiv = document.querySelector("#toy-collection")
+        const cardDiv = document.createElement("div")
+        cardDiv.classList.add("card")
+        cardDiv.id = toyObj.id
+        cardDiv.innerHTML = `
+            <h2>${toyObj.name}</h2>
+            <img src=${toyObj.image} class="toy-avatar" />
+            <p id="likes">${toyObj.likes} likes</p>
+            <button class="like-btn">Like <3</button>
+            <button class="unlike-btn">Unlike </button>
+        `
+        toyCollectionDiv.append(cardDiv)
+    }
+
+     const clickHandler = () => {
+         document.addEventListener("click", function(e) {
+            if (e.target.matches(".like-btn")) {
+                let button = e.target
+                let parentDiv = button.parentElement
+                let toyId = parentDiv.id
+                let likeSpan = parentDiv.querySelector("#likes") 
+                let currentLikes = parseInt(likeSpan.innerText.split(' ')[0])
+                // debugger
+
+                let newLikes = currentLikes + 1
+    
+                const configObj = {
+                    method: "PATCH",
+                    headers: {
+                        "content-type": "application/json",
+                        "accept": "application/json"
+                    },
+                    body: JSON.stringify({likes: newLikes})
+                }
+                    fetch(toysUrl + toyId, configObj)
+                    .then(response => response.json())
+                    .then(updatedLikesObj => {console.log(likeSpan.innerHTML = `${updatedLikesObj.likes} likes`)})
+                } else if (e.target.matches(".unlike-btn")){
+                let button = e.target
+                let parentDiv = button.parentElement
+                let toyId = parentDiv.id
+                let likeSpan = parentDiv.querySelector("#likes") 
+                let currentLikes = parseInt(likeSpan.innerText.split(' ')[0])
+                // debugger
+
+                let newLikes = currentLikes - 1
+    
+                const configObj = {
+                    method: "PATCH",
+                    headers: {
+                        "content-type": "application/json",
+                        "accept": "application/json"
+                    },
+                    body: JSON.stringify({likes: newLikes})
+                }
+                    fetch(toysUrl + toyId, configObj)
+                    .then(response => response.json())
+                    .then(updatedLikesObj => {console.log(likeSpan.innerHTML = `${updatedLikesObj.likes} likes`)})
+                }
+             })
+        }
+
+        const submitHandler = () => {
+            document.addEventListener("submit", function(e){
+                e.preventDefault()
+                const form = document.querySelector("form")
+                
+                const toyObj = {    
+                    name: form.children.name.value,
+                    image: form.children.image.value,
+                    likes: 0
+                }
+
+                const configObj = {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Accept': 'application/json',
+                    },
+                    body: JSON.stringify(toyObj)
+                }
+
+                    fetch(toysUrl, configObj)
+                    .then(response => response.json())
+                    .then(newToy => renderSingleToy(newToy))
+
+                    form.reset()
+            })
+        }
 
 
-function insertCards(toyCard) {
-    let cardHolder = document.getElementById('toy-collection')
-    cardHolder.append(toyCard)
-}
+    submitHandler()
+    clickHandler()
+    getToys()
 
-
-  getToys()
+})
